89fbfb0986c35695c377e76bd337a26f58433395
swag100 <ti.n.tripp@baypath.net>
2024-12-15 09:33:42 -0500

    basic edits

--- components/strumline.py
+++ components/strumline.py
@@ -1,9 +1,17 @@
 import pygame
 
 from components.spritesheet import Spritesheet
+
+#individual receptor
+class StrumlineNote:
+    def __init__(self):
+        self.default_offset = 13 #from og source code
+        
+
+#handles the receptors and the notes (with sustains) for a given player.
 class Strumline:
     def __init__(self, bot_play):
         self.bot_play = bot_play
-        
+
         #for loop, repeat 4 times make each individual strum
         #What is a strum?
