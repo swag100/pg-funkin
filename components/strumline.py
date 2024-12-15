e8cd18e61e7babb5bd306399a7de46a3ef4c1d8a
swag100 <ti.n.tripp@baypath.net>
2024-12-15 09:25:02 -0500

    Added bare strumline class, doesn't do anything yet

--- components/strumline.py
+++ components/strumline.py
@@ -1,1 +1,9 @@
 import pygame
+
+from components.spritesheet import Spritesheet
+class Strumline:
+    def __init__(self, bot_play):
+        self.bot_play = bot_play
+        
+        #for loop, repeat 4 times make each individual strum
+        #What is a strum?
